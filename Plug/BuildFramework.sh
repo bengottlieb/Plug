3c90c41e50f38e591d85cdf19148a7761d5336b2
Ben Gottlieb <ben@standalone.com>
2017-03-12 05:58:53 -0500

    added git_number and updated build date

--- Plug/BuildFramework.sh
+++ Plug/BuildFramework.sh
@@ -20,17 +20,21 @@ MAC_FRAMEWORKS=/Users/ben/Documents/ManagedProjects/Frameworks/Mac_Builds
 GIT_BRANCH=`git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1/"`
 GIT_REV=`git rev-parse --short HEAD`
 
-BUILD_DATE=`date`
+BUILD_DATE=`date "+%c%m%d%H%M%S"`
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+BUILD_NUMBER=`echo $(expr $(git rev-list $BRANCH --count) - $(git rev-list HEAD..$BRANCH --count))`
 
 IOS_PLIST_PATH="${PROJECT_DIR}/Plug/iOS/info.plist"
 /usr/libexec/PlistBuddy "${IOS_PLIST_PATH}" -c "Add :branch string ${GIT_BRANCH}"
 /usr/libexec/PlistBuddy "${IOS_PLIST_PATH}" -c "Add :rev string ${GIT_REV}"
 /usr/libexec/PlistBuddy "${IOS_PLIST_PATH}" -c "Add :built string ${BUILD_DATE}"
+/usr/libexec/PlistBuddy "${IOS_PLIST_PATH}" -c "Add :git_number string ${BUILD_NUMBER}"
 
 MAC_PLIST_PATH="${PROJECT_DIR}/Plug/Mac/info.plist"
 /usr/libexec/PlistBuddy "${MAC_PLIST_PATH}" -c "Add :branch string ${GIT_BRANCH}"
 /usr/libexec/PlistBuddy "${MAC_PLIST_PATH}" -c "Add :rev string ${GIT_REV}"
 /usr/libexec/PlistBuddy "${MAC_PLIST_PATH}" -c "Add :built string ${BUILD_DATE}"
+/usr/libexec/PlistBuddy "${MAC_PLIST_PATH}" -c "Add :git_number string ${BUILD_NUMBER}"
 
 echo "plists updated"
 # make sure the output directory exists
@@ -79,7 +83,9 @@ cp -R "${BASE_BUILD_DIR}/${CONFIG}/${FRAMEWORK_NAME}.framework" "${PROJECT_DIR}/
 $(/usr/libexec/PlistBuddy "${MAC_PLIST_PATH}" -c "Delete :branch" 2> /dev/null)
 $(/usr/libexec/PlistBuddy "${MAC_PLIST_PATH}" -c "Delete :rev" 2> /dev/null)
 $(/usr/libexec/PlistBuddy "${MAC_PLIST_PATH}" -c "Delete :built" 2> /dev/null)
+$(/usr/libexec/PlistBuddy "${MAC_PLIST_PATH}" -c "Delete :git_number" 2> /dev/null)
 
 $(/usr/libexec/PlistBuddy "${IOS_PLIST_PATH}" -c "Delete :branch" 2> /dev/null)
 $(/usr/libexec/PlistBuddy "${IOS_PLIST_PATH}" -c "Delete :rev" 2> /dev/null)
 $(/usr/libexec/PlistBuddy "${IOS_PLIST_PATH}" -c "Delete :built" 2> /dev/null)
+$(/usr/libexec/PlistBuddy "${IOS_PLIST_PATH}" -c "Delete :git_number" 2> /dev/null)
