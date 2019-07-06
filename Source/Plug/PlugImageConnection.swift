b22619ed9634e76af07fed94d40e07b315eaa071
Ben Gottlieb <ben@standalone.com>
2019-07-05 21:45:00 -0500

    updated Mac target

--- Source/Plug/PlugImageConnection.swift
+++ Source/Plug/PlugImageConnection.swift
@@ -10,17 +10,19 @@ import Foundation
 
 #if canImport(UIKit)
     import UIKit
+	 public typealias UXImage = UIImage
 #endif
 
 #if canImport(AppKit)
     import AppKit
+	 public typealias UXImage = NSImage
 #endif
 
 
 extension Connection {
 	public enum ImageConnectionError: Error { case noImageReturned }
 
-    public func fetchImage(completion: @escaping (Result<UIImage, Error>) -> Void) {
+    public func fetchImage(completion: @escaping (Result<UXImage, Error>) -> Void) {
 		self.completion { connection, data in
 			if let image = data.image {
 				completion(.success(image))
